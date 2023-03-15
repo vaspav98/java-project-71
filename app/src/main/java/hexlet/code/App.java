a8423642f6526a917a3d9bd61d96cd96069e0ff1
Pavel Vasiliev <vas-pav@mail.ru>
2023-03-15 14:20:53 +0300

    do the 4th step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -15,6 +15,16 @@ import java.util.concurrent.Callable;
         description = "Compares two configuration files and shows a difference.")
 public class App {
 
+    @Parameters(paramLabel = "filepath1 ", description = "path to first file")
+    File filepath1;
+
+    @Parameters(paramLabel = "filepath2 ", description = "path to second file")
+    File filepath2;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    String format;
+
+
     public static void main(String... args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
